63bb50a00da8e128851e2fe5a4b077f87aed2bbe
HusseinBeygi <husseinbeygi.dev@gmail.com>
2021-12-21 08:48:18 +0330

    Added The PrintingQuote

---
+++ PrintingQuote/QuoteTest.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+
+namespace PrintingQuote
+{
+    public class QuoteTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            string quote = "This is a quote";
+            string author = "Test";
+            string expected = "Test Says :\"This is a quote\"";
+            //Act
+            string actual = GenrateQuote(quote, author);
+
+            //Assert
+
+            Assert.Equal(expected.Trim(), actual.Trim());         
+
+        }
+
+        private string GenrateQuote(string quote, string author)
+        {
+            return author + " Says :" + "\"" + quote + "\"";    
+        }
+    }
+}
