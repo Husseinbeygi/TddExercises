a434e47bd52dccf655d02ba3550435251e256b87
HusseinBeygi <husseinbeygi.dev@gmail.com>
2021-12-22 08:37:51 +0330

    Complete The Madlibs

--- MadLibs/UnitTest1.cs
+++ MadLibs/UnitTest1.cs
@@ -1,13 +1,32 @@
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace MadLibs
 {
     public class UnitTest1
     {
+
         [Fact]
         public void Test1()
         {
+            //  Arrange 
+            string noun = "dog";
+            string verb = "walk";
+            string adjective = "blue";
+            string adverb = "quickly";
 
+            // Act
+            string actual = MakeMadlib(noun, verb, adjective, adverb);
+            string expected = "Do you walk your blue dog quickly? That's hilarious!";
+
+            // Assert
+            Assert.Equal(expected, actual);     
+        }
+
+        private string MakeMadlib(string noun, string verb, string adjective, string adverb)
+        {
+            return $"Do you {verb} your {adjective} {noun} {adverb}? That's hilarious!";
         }
     }
 }
